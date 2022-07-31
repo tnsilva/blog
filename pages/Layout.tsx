e73f15dbf7af9c3008a3e3ed4c35a01015384918
tnsilva <tnsilva175@gmail.com>
2022-07-31 15:15:34 -0300

    add tn logo

--- pages/Layout.tsx
+++ pages/Layout.tsx
@@ -1,43 +1,46 @@
 import Head from 'next/head'
 import Link from 'next/link'
+// import {logoTDev} from
 
 interface LayoutProps {
   children: React.ReactNode
 }
 
- const Layout = ({ children }: LayoutProps) => {
-   return (
-     <>
-       <Head>
-         <title>Meu blog</title>
-         <link rel="icon" href="/favicon.ico" />
-         <meta
-           name="google-site-verification"
-           content="_iCBtcDxhy2qQKdErYKcTaafdZ8LdzvalyqhxwOy0kM"
-         />
-       </Head>
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <>
+      <Head>
+        <title>Meu blog</title>
+        <link rel="icon" href="/logoTDev.svg" />
+        <meta
+          name="google-site-verification"
+          content="_iCBtcDxhy2qQKdErYKcTaafdZ8LdzvalyqhxwOy0kM"
+        />
+        /
+      </Head>
 
-       <header className="bg-gradient-to-r from-blue-500 to-blue-500 py-2 text-center">
-         <Link href="/">
-           <a>
-             <h2 className="text-3xl font-bold text-white">
-               Blog do @thalisson.dev
-             </h2>
-           </a>
-         </Link>
-       </header>
+      <header className="bg-gradient-to-r from-blue-500 to-blue-500 py-2 text-center">
+        <Link href="/">
+          <a>
+            <h2 className="text-3xl font-bold text-white">
+              Blog do @thalisson.dev
+              <img src="/logoTn.png" alt="" width={32} height={32} />
+            </h2>
+          </a>
+        </Link>
+      </header>
 
-       <div className="container mx-auto flex  flex-col px-4">
-         <main className="flex-2 prose mx-6 my-auto rounded p-6  lg:prose-xl">
-           {children}
-         </main>
-       </div>
+      <div className="container mx-auto flex  flex-col px-4">
+        <main className="flex-2 prose mx-6 my-auto rounded p-6  lg:prose-xl">
+          {children}
+        </main>
+      </div>
 
-       {/* <footer>
+      <footer>
         <h2 className="py-2 text-center">Meu blog</h2>
-      </footer> */}
-     </>
-   )
- }
+      </footer>
+    </>
+  )
+}
 
 export default Layout
